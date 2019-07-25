b80e53f02c1640d1a4869893c517c78deb97ec1f
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-07-25 19:00:04 +0530

    Update Exercise06.cpp

--- Chapter02/Exercise06.cpp
+++ Chapter02/Exercise06.cpp
@@ -1,1 +1,35 @@
+// If/Else exercise – Menu Program
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string input;
+    int number;
+    
+    std::cout << "Menu\n";
+    std::cout << "1: Fries\n";
+    std::cout << "2: Burger\n";
+    std::cout << "3: Shake\n";
+    std::cout << "Please enter a number 1-3 to view an item price: ";
+    getline (std::cin, input);
+    number = std::stoi(input);
+    if (number == 1)
+    {
+        std::cout << "Fries: $0.99\n";
+    }
+    else if (number == 2) 
+    {
+        std::cout << "Burger: $1.25\n";
+    }
+    else if (number == 3)
+    {
+        std::cout << "Shake: $1.50\n";
+    }
+    else
+    {
+        std::cout << "Invalid choice.";
+    }  
+        return 0;
+}
 

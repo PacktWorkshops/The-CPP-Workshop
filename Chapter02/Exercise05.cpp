de716b66f387d30e0f22177c98a2bb0f5e13284c
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-07-25 18:58:21 +0530

    Update Exercise05.cpp

--- Chapter02/Exercise05.cpp
+++ Chapter02/Exercise05.cpp
@@ -1,1 +1,22 @@
+// If/Else example 1.
+#include <iostream>
+#include <string>
 
+int main()
+{
+    std::string input;
+    int number;
+    std::cout << "Please enter a number: ";
+    getline (std::cin, input);
+    number = std::stoi(input);
+    if (number < 10)
+    {
+        std::cout << "The number you entered was less than 10!\n";
+    }
+    else if (number > 10) 
+    {
+        std::cout << "The number you entered was greater than 10!\n";
+    }
+    
+    return 0;
+}

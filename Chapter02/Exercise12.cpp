0b6b3d2091fc50132b37335d2bfd273caaa08847
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-07-25 19:22:44 +0530

    Create Exercise12.cpp

---
+++ Chapter02/Exercise12.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    int multiple = 0;
+    int count = 0;
+    int numbersPrinted = 0;
+    std::string input = "";
+    
+    std::cout << "Enter the value whose multiples will be printed: ";
+    getline(std::cin, input);
+    multiple = std::stoi(input);
+    
+    std::cout << "Enter maximum amount of numbers to print: ";
+    getline(std::cin, input);
+    count = std::stoi(input);
+    for (int i = 1; i <= 100; ++i)
+    {
+          if (numbersPrinted == count)
+    {
+        break;
+    }
+    if (i % multiple != 0)
+    {
+        continue;
+    }
+std::cout << i << "\n";
+++numbersPrinted;
+  
+    }

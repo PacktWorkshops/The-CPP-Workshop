aced5d0a852a4eebc7c56e4905db461d5e36565f
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-07-25 19:11:12 +0530

    Update Exercise07.cpp

--- Chapter02/Exercise07.cpp
+++ Chapter02/Exercise07.cpp
@@ -1,1 +1,42 @@
+// If/Else to Switch/Case
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string input;
+    int number;
+    
+    std::cout << "Menu\n";
+    std::cout << "1: Fries\n";
+    std::cout << "2: Burger\n";
+    std::cout << "3: Shake\n";
+    std::cout << "Please enter a number 1-3 to view an item price: ";
+    getline (std::cin, input);
+    number = std::stoi(input);
+        switch (number)
+        {
+        case 1:
+            std::cout << "Fries: $0.99\n";
+        break;
+    switch (number)
+    {
+        case 1:
+            std::cout << "Fries: $0.99\n";
+        break;
+        
+        case 2:
+            std::cout << "Burger: $1.25\n";
+        break;
+        
+        case 3:
+            std::cout << "Shake: $1.50\n";
+        break;
+        
+        default:
+            std::cout << "Invalid choice.";
+        break;
+    }
+}
+}
 

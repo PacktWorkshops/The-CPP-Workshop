6570837d4f02f81855619f372bb823c4b8cc523c
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-08 18:48:13 +0530

    Update Exercise19.cpp

--- Chapter03/Exercise19.cpp
+++ Chapter03/Exercise19.cpp
@@ -1,1 +1,35 @@
-
+// Classes/struct exercise.
+#include <iostream>
+#include <string>
+class MyClass
+{
+public:
+int myInt = 0;
+bool myBool = false;
+std::string GetString()
+{
+return "Hello World!";
+}
+};
+struct MyStruct
+{
+int myInt = 0;
+int myBool = 0;
+std::string GetString()
+{
+return "Hello World!";
+}
+};
+int main()
+{
+MyClass classObject;
+std::cout << "classObject::myInt: " << classObject.myInt << "\n";
+  std::cout << "classObject::myBool: " << classObject.myBool << "\n";
+std::cout << "classObject::GetString: " << classObject.GetString() <<
+"\n";
+MyStruct structObject;
+std::cout << "\nstructObject::myInt: " << structObject.myInt << "\n";
+std::cout << "structObject::myBool: " << structObject.myBool << "\n";
+std::cout << "structbject::GetString: " << structObject.GetString() <<
+"\n";
+}

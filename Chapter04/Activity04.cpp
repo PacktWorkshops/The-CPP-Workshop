a471a6dca2ac309e8da4811a46fc607b09f5b56d
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-08 20:02:18 +0530

    Update Activity04.cpp

--- Chapter04/Activity04.cpp
+++ Chapter04/Activity04.cpp
@@ -1,1 +1,27 @@
-
+// Activity 4: Fizz Buzz.
+#include <iostream>
+int main()
+{
+  for (int i = 1; i <= 100; ++i)
+{
+    bool multiple = false;
+    if (i % 3 == 0)
+{
+std::cout << "Fizz";
+multiple = true;
+}
+    if (i % 5 == 0)
+{
+std::cout << "Buzz";
+multiple = true;
+}
+    if (!multiple)
+{
+std::cout << i;
+}
+    if (i < 100)
+{
+std::cout << ", ";
+}
+}
+}

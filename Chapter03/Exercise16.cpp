390d7cb1dbb77b5891f9cb6f6082fcc6b090d12f
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-08 18:39:29 +0530

    Update Exercise16.cpp

--- Chapter03/Exercise16.cpp
+++ Chapter03/Exercise16.cpp
@@ -1,1 +1,25 @@
-
+// Vector example.
+#include <iostream>
+#include <string>
+#include <vector>
+std::vector<int> myVector {1, 2, 3, 4, 5};
+void PrintVector()
+{
+for (int i = 0; i < myVector.size(); ++i)
+{
+std::cout << myVector[i];
+}
+std::cout << "\n\n";
+}
+int main()
+{
+PrintVector();
+myVector.pop_back();
+PrintVector();
+myVector.push_back(6);
+PrintVector();
+myVector.erase(myVector.begin() + 1);
+PrintVector();
+myVector.insert(myVector.begin() + 3, 8);
+PrintVector();
+}

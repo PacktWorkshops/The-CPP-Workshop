9ccf38d0bb44a9e0d845542f0e6134784ae7bb48
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-08 18:39:14 +0530

    Update Exercise15.cpp

--- Chapter03/Exercise15.cpp
+++ Chapter03/Exercise15.cpp
@@ -1,1 +1,17 @@
-
+// Vector example.
+#include <iostream>
+#include <string>
+#include <vector>
+std::vector<int> myVector;
+void PrintVector()
+{
+for (int i = 0; i < myVector.size(); ++i)
+{
+std::cout << myVector[i];
+}
+std::cout << "\n\n";
+}
+int main()
+{
+PrintVector();
+}

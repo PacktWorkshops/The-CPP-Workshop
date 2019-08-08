7ad9303fbe376cdcf43cad3fb5bfb71f2fbb3cd3
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-08 18:44:08 +0530

    Update Exercise18.cpp

--- Chapter03/Exercise18.cpp
+++ Chapter03/Exercise18.cpp
@@ -1,1 +1,19 @@
-
+// Accessibility example.
+#include <iostream>
+#include <string>
+class MyClass
+{
+public:
+int myPublicInt = 0;
+protected:
+int myProtectedInt = 0;
+private:
+int myPrivateInt = 0;
+};
+int main()
+{
+MyClass testClass;
+std::cout << testClass.myPublicInt << "\n";
+std::cout << testClass.myProtectedInt << "\n";
+std::cout << testClass.myPrivateInt << "\n";
+}

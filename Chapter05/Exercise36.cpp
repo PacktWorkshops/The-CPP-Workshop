dd3b7d8e20bed92ea6955d84e131cdf3efe56648
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-12 11:45:12 +0530

    Update Exercise36.cpp

--- Chapter05/Exercise36.cpp
+++ Chapter05/Exercise36.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+using namespace std;
+
+struct mydata
+{
+    char const* name_;
+    bool darkside_;
+    mydata (char const* name, bool dark) 
+    { 
+        name_ = name; darkside_ = dark; 
+    }
+};
+mydata cast[3]
+{
+    { "Darth Vader", true },
+    { "Luke Skywalker", false },
+    { "Han Solo", false }
+};
+
+void printname(mydata& data)
+{
+    cout << "Hello. I am " << data.name_ << endl;
+    if (data.darkside_)
+        cout << "I was seduced by the dark side" << endl;
+}
+
+int main()
+{
+    for (mydata& data : cast)
+    {
+        printname(data);
+    }
+    return 0;
+}
 

9208f47586fabc23a8362fe36caa032d280d04f2
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-12 12:36:31 +0530

    Update Exercise59.cpp

--- Chapter08/Exercise59.cpp
+++ Chapter08/Exercise59.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+
+class Track
+{ 
+  public:
+  // m_ prefix added to member variables, to avoid naming conflicts with
+
+float m_lengthInSeconds;
+string m_trackName;
+  Track(float lengthInSeconds, string trackName)
+{
+m_lengthInSeconds = lengthInSeconds;
+m_trackName = trackName;
+}
+};
+int main()
+{
+Track track(200.0f, "Still Alive");
+cout << "Track Name = " << track.m_trackName << endl;
+cout << "Track Length = " << track.m_lengthInSeconds << endl;
+return 0;
+}

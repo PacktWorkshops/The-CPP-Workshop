f86dc89dfef880f627e32e70ec99591f331fb609
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-12 11:43:54 +0530

    Update Exercise34.cpp

--- Chapter05/Exercise34.cpp
+++ Chapter05/Exercise34.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void copychars(char* from, char* to, int count)
+{
+    if (from == nullptr || to == nullptr)
+        return;
+        
+    while (count-- > 0)
+    {
+        *to++ = *from++;
+    }
+}
+
+int main()
+{
+    char string[] { "uvwxyz" };
+    char buffer[10];
+    copychars (string, buffer, 7);
+    cout << buffer << endl;
+    return 0;
+}
+
 

3a534f909a8692b67bdff4113c81f8a87aa5cea1
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-12 15:39:23 +0530

    Update Exercise69.cpp

--- Chapter10/Exercise69.cpp
+++ Chapter10/Exercise69.cpp
@@ -1,1 +1,40 @@
-
+#include <iostream>
+#include <string>
+class Shape
+{
+public:
+virtual int CalculateArea() = 0;
+protected:
+int area = 0;
+};
+class Square : public Shape
+{
+public:
+int height = 10;
+int CalculateArea() override
+{
+area = height * height;
+return area;
+}
+};
+class Circle : public Shape
+{
+public:
+int radius = 10;
+int CalculateArea() override
+{
+area = 3.14 * (radius * radius);
+return area;
+}
+};
+int main()
+{
+Shape* square = new Square();
+Shape* circle = new Circle();
+std::cout << "Square Area: " << square->CalculateArea() << std::endl;
+std::cout << "Circle Area: " << circle->CalculateArea() << std::endl;
+delete square;
+square = nullptr;
+delete circle;
+circle = nullptr;
+}

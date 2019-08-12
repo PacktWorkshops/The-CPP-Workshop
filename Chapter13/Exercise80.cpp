a7796f12916874edcb21518987e20a7469c21e5c
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-12 15:50:32 +0530

    Update Exercise80.cpp

--- Chapter13/Exercise80.cpp
+++ Chapter13/Exercise80.cpp
@@ -1,1 +1,19 @@
-
+#include <iostream>
+using namespace std;
+int deeply_nested()
+{
+throw 123;
+cout << "in deeply_nested() after throw" << endl;
+return 0;
+}
+int intermediate()
+{
+int rc = deeply_nested();
+cout << "in intermediate(), after deeply_nested()";
+return rc;
+}
+int main()
+{
+intermediate();
+return 0;
+}

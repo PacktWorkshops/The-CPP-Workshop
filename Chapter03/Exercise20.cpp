9c9fa5e57f399e91a2a3e5c84bcc7a358fd1b9d4
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-12 19:12:29 +0530

    Update Exercise20.cpp

--- Chapter03/Exercise20.cpp
+++ Chapter03/Exercise20.cpp
@@ -1,81 +1,16 @@
-// Activity 3: SignUp Application.
 #include <iostream>
-#include <string>
-#include <vector>
-#include <stdexcept>
-struct Person
+void MyFunc()
 {
-int age = 0;
-std::string name = "";
-};
-std::vector<Person> records;
-void AddRecord(std::string newName, int newAge)
-{
-Person newRecord;
-newRecord.name = newName;
-newRecord.age = newAge;
-records.push_back(newRecord);
-std::cout << "\nUser record added successfully.\n\n";
-};
-Person FetchRecord(int userID)
-{
-return records.at(userID);
-};
+int myInt1 = 1;
+}
 int main()
 {
-std::cout << "User SignUp Application\n" << std::endl;
-bool bIsRunning = true;
-while (bIsRunning)
+int myInt2 = 2;
 {
-std::cout << "Please select an option:\n";
-std::cout << "1: Add Record\n";
-std::cout << "2: Fetch Record\n";
-std::cout << "3: Quit\n\n";
-std::cout << "Enter option: ";
-std::string inputString;
-std::getline(std::cin, inputString);
-// Determine user selection.
-switch (std::stoi(inputString))
-{
-case 1:
-{
-std::string name = "";
-int age = 0;
-std::cout << "\nAdd User. Please enter user name and age:\n";
-std::cout << "Name: ";
-std::getline(std::cin, name);
-std::cout << "Age: ";
-std::getline(std::cin, inputString);
-age = std::stoi(inputString);
-AddRecord(name, age);
+int myInt3 = 3;
 }
-break;
-case 2:
-{
-int userID = 0;
-std::cout << "\nPlease enter user ID:\n";
-std::cout << "User ID: ";
-std::getline(std::cin, inputString);
-userID = std::stoi(inputString);
-Person person;
-try
-{
-person = FetchRecord(userID);
-}
-catch (const std::out_of_range& oor) {
-std::cout << "\nError: Invalid UserID.\n\n";
-break;
-}
-std::cout << "User Name: " << person.name << "\n";
-std::cout << "User Age: " << person.age << "\n\n";
-}
-break;
-case 3:
-bIsRunning = false;
-break;
-default:
-std::cout << "\n\nError: Invalid option selection.\n\n";
-break;
-}
-}
+// print values
+std::cout << myInt1 << std::endl;
+std::cout << myInt2 << std::endl;
+std::cout << myInt3 << std::endl;
 }

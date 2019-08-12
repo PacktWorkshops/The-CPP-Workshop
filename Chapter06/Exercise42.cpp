81000505f92f60218093135a42a016338ca6aafc
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-12 11:52:55 +0530

    Update Exercise42.cpp

--- Chapter06/Exercise42.cpp
+++ Chapter06/Exercise42.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char* p = new char[10];
+    p[0] = '!';
+    delete[] p;
+    cout << "p[0] = " << p[0] << endl;
+    return 0;
+}
 

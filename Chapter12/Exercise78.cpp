6a7dde27ade2c2733e1aa10bad67af9e3f2b69ee
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-12 18:24:04 +0530

    Update Exercise78.cpp

--- Chapter12/Exercise78.cpp
+++ Chapter12/Exercise78.cpp
@@ -1,1 +1,37 @@
-
+#include <iostream>
+#include <string>
+#include <set>
+using namespace std;
+class Person
+{
+public:
+Person(string name, int age)
+{
+m_name = name;
+m_age = age;
+}
+string getName() const { return m_name; }
+int getAge() const { return m_age; }
+private:
+string m_name;
+int m_age;
+};
+struct customComparator
+{
+bool operator() (const Person& a, const Person& b) const
+{
+return (a.getAge() > b.getAge());
+}
+};
+int main()
+{
+set<Person, customComparator> personSet;
+Person a("bob", 35);
+Person b("bob", 25);
+personSet.insert(a);
+personSet.insert(b);
+for(auto person : personSet)
+{
+cout << person.getAge() << endl;
+}
+}

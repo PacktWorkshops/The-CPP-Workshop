7d802c16ee9c8c38061470fac63151713cfb352f
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-12 12:13:32 +0530

    Update Exercise57.cpp

--- Chapter07/Exercise57.cpp
+++ Chapter07/Exercise57.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+struct noisy
+{
+    noisy() { cout << "constructing noisy" << endl; }
+   ~noisy() { cout << "destroying noisy" << endl; }
+};
+ 
+int main()
+{
+    shared_ptr<noisy> u1(new noisy);
+    shared_ptr<noisy> u2 = make_shared<noisy>();
+    shared_ptr<noisy> u3 = u2;
+    u2.reset();
+    return 0;
+}
 

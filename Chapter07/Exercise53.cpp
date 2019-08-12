49737d05aa8a614515b40b43b31f333e41c4625a
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-12 12:10:40 +0530

    Update Exercise53.cpp

--- Chapter07/Exercise53.cpp
+++ Chapter07/Exercise53.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+struct noisy
+{
+    noisy() { cout << "constructing noisy" << endl; }
+   ~noisy() { cout << "destroying noisy" << endl; }
+};
+ 
+int main()
+{
+    unique_ptr<noisy> u1(new noisy);
+    unique_ptr<noisy> u2 = make_unique<noisy>();
+    auto u3 = make_unique<noisy[]>(4);
+    return 0;
+}
 

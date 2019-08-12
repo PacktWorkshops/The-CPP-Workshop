03b7bc468bbcf72da94ad2e400f3a782f8d1cb97
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-12 11:38:02 +0530

    Update Exercise28.cpp

--- Chapter05/Exercise28.cpp
+++ Chapter05/Exercise28.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int a[7]{ 1, 3, 5, 4, 2, 9, -1 };
+   int *p = nullptr;
+   
+   p = &a[2];
+   cout << "*p = " << *p << ", a[2] = " << a[2] << endl;
+   
+   cout << "p[3] = " << p[3] << ", a[5] = " << a[5] << endl;
+
+   return 0;
+}
 

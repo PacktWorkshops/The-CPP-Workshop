03b61195d2fe59a39adf7bd279a27a190dcbf858
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-12 11:38:26 +0530

    Update Exercise29.cpp

--- Chapter05/Exercise29.cpp
+++ Chapter05/Exercise29.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int numbers[5]{ 0, 100, 200, 300, 400 };
+   int* pint = numbers;
+   int* p2 = &numbers[3];
+
+   cout << "pint = " << pint << ", pint+1 = " << pint+1 
+        << ", sizeof(int) = " << sizeof(int) << endl;
+        
+   cout << "*(pint+1) = " << *(pint+1)
+        << ", pint[1] = " << pint[1] << endl;
+        
+   cout << "*(pint+4) = " << *(pint+4)
+        << ", pint[4] = " << pint[4] << endl;
+        
+   cout << "p2 - pint = " << p2 - pint << endl;
+   
+   cout << "p2 == pint = " << boolalpha << (p2 == pint) << endl;
+   cout << "p2 > pint = " << boolalpha << (p2 > pint) << endl;
+   return 0;
+}
 

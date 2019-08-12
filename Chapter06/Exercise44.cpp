e55786d96f8fbe8aaa0730d1ec228c2b82cc71a2
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-12 11:55:07 +0530

    Update Exercise44.cpp

--- Chapter06/Exercise44.cpp
+++ Chapter06/Exercise44.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class noisy
+{
+    int i_;
+public:
+    noisy(int i) : i_(i)
+    { 
+        cout << "constructing noisy " << i << endl; 
+    }
+   ~noisy() 
+    { 
+        cout << "destroying noisy " << i_ << endl;
+    }
+};
+
+int main()
+{
+    noisy N(1);
+    noisy* p = new noisy(2);
+    p = new noisy(3);
+    delete p;
+
+    return 0;
+}
 

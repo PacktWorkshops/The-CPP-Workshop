cbdb043acc7386fa60dd0742ac016ef1a3903ee2
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-12 11:51:47 +0530

    Update Exercise40.cpp

--- Chapter06/Exercise40.cpp
+++ Chapter06/Exercise40.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
+
+struct noisy
+{
+    noisy() { cout << "constructing noisy" << endl; }
+   ~noisy() { cout << "destroying noisy" << endl; }
+};
+
+int main()
+{
+    cout << "getting a noisy array" << endl;
+    noisy* pnoisy = new noisy[3];
+    cout << "deleting noisy array" << endl;
+    delete[] pnoisy;
+    
+    return 0;
+}
 

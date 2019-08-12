6b5f7999e3e5e277a0d316c192cabe95ab6eaada
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-12 15:36:06 +0530

    Update Exercise67.cpp

--- Chapter10/Exercise67.cpp
+++ Chapter10/Exercise67.cpp
@@ -1,1 +1,29 @@
-
+#include <iostream>
+#include <string>
+class Shape
+{
+public:
+int area = 0;
+  protected:
+std::string color = "";
+private:
+bool hasOutline = false;
+};
+class Square : private Shape
+{
+public:
+Square()
+{
+area = 5;
+color = "red";
+hasOutline = true;
+};
+};
+int main()
+{
+Square mySquare;
+std::cout << "Square Area: " << mySquare.area << std::endl;
+std::cout << "Square Color: " << mySquare.color << std::endl;
+std::cout << "Square Has Outline: " << mySquare.hasOutline <<
+std::endl;
+}

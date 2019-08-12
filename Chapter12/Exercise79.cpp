eff2e017821b2893700226834c11a3da366993f4
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-12 18:25:15 +0530

    Update Exercise79.cpp

--- Chapter12/Exercise79.cpp
+++ Chapter12/Exercise79.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <string>
+#include <set>
+#include <stdlib.h>
+using namespace std;
+int main()
+{
+set<int> intSet;
+multiset<int> intMultiset;
+  for(unsigned int i = 0; i < 100; i++)
+{
+intMultiset.insert(1 + rand() % 100);
+}
+  for(auto i : intMultiset)
+{
+intSet.insert(i);
+}
+  cout << "there are " << intSet.size() << " unique elements in the multiset";
+}
 

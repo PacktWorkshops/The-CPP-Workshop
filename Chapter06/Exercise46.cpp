7b3696a1aac90d0ef7ea02e4bd0c2a27c9018ea8
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-12 11:56:55 +0530

    Update Exercise46.cpp

--- Chapter06/Exercise46.cpp
+++ Chapter06/Exercise46.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
 
+struct noisy
+{
+    noisy() { cout << "constructing noisy" << endl; }
+   ~noisy() { cout << "destroying noisy" << endl; }
+};
+
+int main()
+{
+    noisy* p = new noisy[3];
+    delete p;
+    return 0;
+}

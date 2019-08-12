68d753f3e369087198c48559ca4f6887b871b4f8
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-12 12:12:29 +0530

    Update Exercise56.cpp

--- Chapter07/Exercise56.cpp
+++ Chapter07/Exercise56.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <memory>
+using namespace std;
+
+shared_ptr<char[]> func(shared_ptr<char[]> str)
+{
+    if (!str)
+    {
+        str.reset(new char[1]);
+        str[0] = '\0';
+    }
+    return str;
+}
+
+int main()
+{
+    shared_ptr<char[]> null;
+    if (null.get() == nullptr)
+        cout << "null is equal to nullptr" << endl;
+    auto result1 = func(null);
+    if (result1.get() == nullptr)
+        cout << "result1 is equal to nullptr" << endl;
+    result1 = func(result1);
+
+    return 0;
+}
 

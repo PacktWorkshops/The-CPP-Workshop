ed9b7c8d09daf83bfb9666b7f499e34d90f4f569
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-12 11:39:59 +0530

    Update Exercise31.cpp

--- Chapter05/Exercise31.cpp
+++ Chapter05/Exercise31.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{    char* alphabet[26] 
+    {
+        "alpha",
+        "bravo",
+        "charlie",
+        "delta",
+        "echo",
+        "foxtrot"
+    }; 
+    for (char **p = alphabet; *p != nullptr; ++p)
+    {
+        cout << *p << " ";
+    }
+    cout << endl; 
+
+    return 0;
+}
 

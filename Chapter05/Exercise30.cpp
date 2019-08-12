279910360d0594e302a58c0f898e9c84e06c13a7
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-12 11:38:48 +0530

    Update Exercise30.cpp

--- Chapter05/Exercise30.cpp
+++ Chapter05/Exercise30.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    int a[5]{ 10, 20, 30, 40, 50 };
+    int* p;
+    for (p = &a[0]; p < &a[5]; p = p + 1)
+    {
+        cout << *p << " ";
+    }
+    cout << endl;
+    return 0;
+}

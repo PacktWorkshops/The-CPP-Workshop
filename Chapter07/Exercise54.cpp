d779840099b2ac49f1c2b619de8961d7e8ef5574
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-12 12:11:25 +0530

    Update Exercise54.cpp

--- Chapter07/Exercise54.cpp
+++ Chapter07/Exercise54.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+#include <memory>
+#include <cstring>
+
+using namespace std;
+
+struct noisy
+{
+    noisy() { cout << "constructing noisy" << endl; }
+   ~noisy() { cout << "destroying noisy" << endl; }
+};
+
+class autobuf
+{
+    unique_ptr<noisy> np_;
+    unique_ptr<char[]> ptr_;
+public:
+    autobuf(char const* str);
+    char* get();
+};
+
+autobuf::autobuf(char const* str)
+  : np_(make_unique<noisy>()),
+    ptr_(make_unique<char[]>(strlen(str) + 1))
+{
+    strcpy(ptr_.get(), str);
+}
+
+char* autobuf::get() 
+{
+    return ptr_.get();
+}
+
+int main()
+{
+   autobuf buffer("my favorite test string");
+   cout << "Hello World! " << buffer.get() << endl; 
+   
+   return 0;
+}
 

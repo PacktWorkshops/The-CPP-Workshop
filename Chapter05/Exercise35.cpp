b5abe349f8f81c2b4b0ba0e50f1193890decdfd3
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-12 11:44:27 +0530

    Update Exercise35.cpp

--- Chapter05/Exercise35.cpp
+++ Chapter05/Exercise35.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+using namespace std;
+
+struct mydata
+{
+    char const* name_;
+    bool hero_;
+};
+mydata heroes[]
+{
+    { "Spider Man", true },
+    { "The Joker", false },
+    { "Doctor Octopus", false },
+    { "Thor", true },
+    { "Batman", true },
+    { "Loki", false }
+};
+
+void printdata(mydata* p)
+{
+    cout << "Hello. I am " << (*p).name_ << ". ";
+    if (p->hero_)
+        cout << "I am a hero." << endl;
+    else
+        cout << "I am a villain." << endl;
+}
+
+int main()
+{
+    cout << sizeof(mydata) << " " << sizeof(mydata*) << endl;
+    
+    for (mydata* p = heroes; p < heroes + 6; ++p)
+    {
+        printdata(p);
+    }
+    return 0;
+}
 

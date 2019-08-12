881150156b4f68175856ddc965851902d82ff117
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-12 12:09:13 +0530

    Update Exercise51.cpp

--- Chapter07/Exercise51.cpp
+++ Chapter07/Exercise51.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+struct noisy_and_big
+{
+    noisy_and_big() { cout << "constructing noisy" << endl; }
+   ~noisy_and_big() { cout << "destroying noisy" << endl; }
+    char big_buffer_[10000];
+};
+
+noisy_and_big* get_noisy_and_big(char const* str)
+{
+    noisy_and_big* ownedp = new noisy_and_big;
+    strcpy(ownedp->big_buffer_, str);
+}
+
+int main()
+{
+    noisy_and_big* newownedp = get_noisy_and_big("a big, big buffer");
+    cout << "noisy and big: " << newownedp->big_buffer_ << endl;
+    
+    delete newownedp;
+
+    return 0;
+}
 

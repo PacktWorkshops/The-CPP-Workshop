e68db5994c2b7886a1481e162adea10ae9bafb97
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-12 14:30:04 +0530

    Update Exercise63.cpp

--- Chapter09/Exercise63.cpp
+++ Chapter09/Exercise63.cpp
@@ -1,29 +1,59 @@
-#include <iostream>
-#include <cmath>
-class Position
-{
-public:
-Position(float x, float y) : m_x(x), m_y(y) {}
-float distance(float x, float y)
-{
-// we must return something at this point if we want it to
-compile
-return 0;
-}
-private:
-float m_x;
-float m_y;
-};
-float distance(float x, float y)
-{
-float xDiff = x - m_x;
-float yDiff = y - m_y;
-return std::sqrt(((xDiff * xDiff) + (yDiff * yDiff)));
-}
-int main()
-{
-Position pos(10.0f, 20.0f);
-std::cout << "The distance from pos to (100, 40) is:" << pos.
-distance(100.0f, 40.0f) << std::endl;
-return 0;
-}
+#include <iostream> 
+
+#include <cmath> 
+
+ 
+
+class Position 
+
+{ 
+
+    public: 
+
+        Position(float x, float y) : m_x(x), m_y(y) {} 
+
+         
+
+        float distance(float x, float y) 
+
+        { 
+
+            float xDiff = x - m_x; 
+
+            float yDiff = y - m_y; 
+
+             
+
+            return std::sqrt(((xDiff * xDiff) + (yDiff * yDiff))); 
+
+        } 
+
+         
+
+    private: 
+
+         
+
+        float m_x; 
+
+        float m_y; 
+
+}; 
+
+ 
+
+int main() 
+
+{ 
+
+    Position pos(10.0f, 20.0f); 
+
+     
+
+    std::cout << "The distance from pos to (100, 40) is:" << pos.distance(100.0f, 40.0f) << std::endl; 
+
+     
+
+    return 0; 
+
+} 

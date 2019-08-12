64fccb8f7ba0284728fd951944d7d90c9fd6c5b7
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-12 16:08:56 +0530

    Update Exercise73.cpp

--- Chapter12/Exercise73.cpp
+++ Chapter12/Exercise73.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+ 
+int main()
+{
+    string str;
+    string str1("Hello, I'm a string!");
+    string str2(str1);
+    string str3(str1, 0, 5);
+    string str4("Hello, I'm a string!", 0, 5);
+    string str5(10, 'x');
+    string str6(str4.begin() + 1, str4.end() - 1);
+   
+    cout << str << endl;
+    cout << str1 << endl;
+    cout << str2 << endl;
+    cout << str3 << endl;
+    cout << str4 << endl;
+    cout << str5 << endl;
+    cout << str6 << endl;
+    return 0;
+}
 

e0d0cfd6814c7278ec22fcdd0135e7ea7a73320c
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-12 11:36:20 +0530

    Update Exercise26.cpp

--- Chapter05/Exercise26.cpp
+++ Chapter05/Exercise26.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+   int i = 12345;
+   int *p = &i;
+   cout << "p = " << p << ", &i = " << &i << endl;
+
+   cout << "i = " << i << endl;
+   *p = *p + 2;
+   cout << "i = " << i << endl; 
+   
+   return 0;
+}

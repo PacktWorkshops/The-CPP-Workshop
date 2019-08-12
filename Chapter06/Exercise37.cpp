3f8a6673aa282752dd6eb203338e485d399b9727
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-12 11:49:01 +0530

    Update Exercise37.cpp

--- Chapter06/Exercise37.cpp
+++ Chapter06/Exercise37.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int* pint = nullptr;
+    pint = new int;
+    cout << "pint = " << pint << endl;
+    delete pint;
+    cout << "pint = " << pint << endl;
+    
+    pint = new int{33333};
+    cout << "*pint = " << *pint << endl;
+    delete pint;
+    
+    return 0;
+}
 

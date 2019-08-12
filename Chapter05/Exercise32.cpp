01f02afbcd34cc0429128541d970d90d45d22e6f
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-12 11:40:47 +0530

    Update Exercise32.cpp

--- Chapter05/Exercise32.cpp
+++ Chapter05/Exercise32.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 10;
+    int& ir = i;
+    i = i + 10;
+    ir = ir * 10;
+    cout << "i = " << i << endl;
+    int* ip = &ir;
+    *ip = 33;
+    cout << "i = " << i << ", *ip = " << *ip
+         << ", ir = " << ir << endl;
+    return 0;
+}
 

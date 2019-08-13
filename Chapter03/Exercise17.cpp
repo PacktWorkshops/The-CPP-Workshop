b781ef9731b162887fd1d926a9f3b9b40d3974a5
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-13 18:33:48 +0530

    Update Exercise17.cpp

--- Chapter03/Exercise17.cpp
+++ Chapter03/Exercise17.cpp
@@ -5,21 +5,21 @@
 std::vector<int> myVector {1, 2, 3, 4, 5};
 void PrintVector()
 {
-for (int i = 0; i < myVector.size(); ++i)
-{
-std::cout << myVector[i];
-}
-std::cout << "\n\n";
-}
+   for (int i = 0; i < myVector.size(); ++i)
+     {
+       std::cout << myVector[i];
+     }
+   std::cout << "\n\n";
+ }
 int main()
 {
-PrintVector();
-myVector.pop_back();
-PrintVector();
-myVector.push_back(6);
-PrintVector();
-myVector.erase(myVector.begin() + 1);
-PrintVector();
-myVector.insert(myVector.begin() + 3, 8);
-PrintVector();
-}
+    PrintVector();
+    myVector.pop_back();
+    PrintVector();
+    myVector.push_back(6);
+    PrintVector();
+    myVector.erase(myVector.begin() + 1);
+    PrintVector();
+    myVector.insert(myVector.begin() + 3, 8);
+    PrintVector();
+ }

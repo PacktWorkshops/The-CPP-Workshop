b78de8bcb9e4117021f1c648ad4e60e54f3f81a1
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-13 15:03:08 +0530

    Update Exercise22.cpp

--- Chapter04/Exercise22.cpp
+++ Chapter04/Exercise22.cpp
@@ -1,38 +1,42 @@
+// Time of Day Calculator.
+#include <iostream>
+#include <string>
+int main() {
+    std::cout << "***Time of Day Calculator***\n";
+    std::cout << "Enter time in military format. eg. (1800, 1430)\n\n";
+    std::cout << "Enter time: ";
+    std::string input;
+    getline(std::cin, input);
+    int time = std::stoi(input);
 
+    if (time < 0000 || time > 2400)
+    {
+          std::cout << "Invalid time.";
+          return 0; 
+    }
 
-std::cout << "***Time of Day Calculator***\n";
-std::cout << "Enter time in military format. eg. (1800, 1430)\n\n";
-std::cout << "Enter time: ";
-std::string input;
-getline(std::cin, input);
-int time = std::stoi(input);
-if (time < 0000 || time > 2400)
-{
-std::cout << "Invalid time.";
-return 0;
-}
-if (time == 0000)
-{
-std::cout << "It's currently midnight.";
-}
-else if (time == 1200)
-{
-std::cout << "It's currently noon.";
-}
-else if (time >= 0600 && time < 1200)
-{
-std::cout << "It's currently morning.";
-}
-else if (time > 1200 && time <= 1700)
-{
-std::cout << "It's currently afternoon.";
-}
-else if (time > 1700 && time <= 2000)
-{
-std::cout << "It's currently evening.";
-}
-else if (time > 2000 || time < 0600)
-{
-std::cout << "It's currently night.";
-}
+    if (time == 0000)
+    {
+        std::cout << "It's currently midnight.";
+    }
+    else if (time == 1200)
+    {
+        std::cout << "It's currently noon.";
+    }
+    else if (time >= 0600 && time < 1200)
+    {
+        std::cout << "It's currently morning.";
+    }
+              else if (time > 1200 && time <= 1700)
+    {
+        std::cout << "It's currently afternoon.";
+    }
+    else if (time > 1700 && time <= 2000)
+    {
+        std::cout << "It's currently evening.";
+    }
+    else if (time > 2000 || time < 0600)
+    {
+        std::cout << "It's currently night.";
+    }
 }

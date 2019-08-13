c038f7bc724782db5a280cb6090100050f704df0
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-13 19:24:13 +0530

    Update Exercise58.cpp

--- Chapter08/Exercise58.cpp
+++ Chapter08/Exercise58.cpp
@@ -6,21 +6,20 @@ using namespace std;
 class Track
 { 
   public:
-  float lengthInSeconds;
-  string trackName;
-  Track ()
-  {
- lengthInSeconds = 0.0f; 
-trackName = "not set"; 
-}
+    float lengthInSeconds;
+    string trackName;
+    Track ()
+     {
+       lengthInSeconds = 0.0f; 
+       trackName = "not set"; 
+     }
 };
+
 int main()
 {
    Track track;
-  
    cout << "Track Name = " << track.trackName << endl;
    cout << "Track Length = " << track.lengthInSeconds << endl;
-
    return 0;
 }
 

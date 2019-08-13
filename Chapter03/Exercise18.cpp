83cf41a6725b4bdb9cf05c80268f0c5a24669520
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-13 18:35:58 +0530

    Update Exercise18.cpp

--- Chapter03/Exercise18.cpp
+++ Chapter03/Exercise18.cpp
@@ -3,17 +3,18 @@
 #include <string>
 class MyClass
 {
-public:
-int myPublicInt = 0;
-protected:
-int myProtectedInt = 0;
-private:
-int myPrivateInt = 0;
-};
+   public:
+     int myPublicInt = 0;
+     protected:
+     int myProtectedInt = 0;
+   private:
+     int myPrivateInt = 0;
+ };
+
 int main()
 {
-MyClass testClass;
-std::cout << testClass.myPublicInt << "\n";
-std::cout << testClass.myProtectedInt << "\n";
-std::cout << testClass.myPrivateInt << "\n";
+   MyClass testClass;
+   std::cout << testClass.myPublicInt << "\n";
+   std::cout << testClass.myProtectedInt << "\n";
+   std::cout << testClass.myPrivateInt << "\n";
 }

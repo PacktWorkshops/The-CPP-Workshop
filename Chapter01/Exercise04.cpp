7932be27fa018900588f4715ff011df9d8b87853
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-13 17:26:30 +0530

    Update Exercise04.cpp

--- Chapter01/Exercise04.cpp
+++ Chapter01/Exercise04.cpp
@@ -1,21 +1,21 @@
 int Max(int a, int b)
 {
-if (a > b)
-{
-return a;
-}
-else
-  {
-return b;
-}
+  if (a > b)
+    {
+      return a;
+    }
+  else
+    {
+      return b;
+    }
 }
 int main()
 {
-int value1 = 0;
-int value2 = 0;
-std::cout << "Please input number 1: ";
-std::cin >> value1;
-std::cout << "Please input number 2: ";
-std::cin >> value2;
+  int value1 = 0;
+  int value2 = 0; 
+  std::cout << "Please input number 1: ";
+  std::cin >> value1;
+  std::cout << "Please input number 2: ";
+  std::cin >> value2;
   std::cout << "The highest number is " << Max(value1, value2);
-};
+}

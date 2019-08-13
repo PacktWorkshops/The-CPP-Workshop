333333654b78af1eb56bf47fa5ec1861b1b8af10
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-13 18:49:29 +0530

    Update Activity04.cpp

--- Chapter04/Activity04.cpp
+++ Chapter04/Activity04.cpp
@@ -3,25 +3,25 @@
 int main()
 {
   for (int i = 1; i <= 100; ++i)
-{
-    bool multiple = false;
-    if (i % 3 == 0)
-{
-std::cout << "Fizz";
-multiple = true;
-}
-    if (i % 5 == 0)
-{
-std::cout << "Buzz";
-multiple = true;
-}
-    if (!multiple)
-{
-std::cout << i;
-}
-    if (i < 100)
-{
-std::cout << ", ";
-}
-}
+   {
+      bool multiple = false;
+      if (i % 3 == 0)
+        {
+           std::cout << "Fizz";
+           multiple = true;
+        }
+      if (i % 5 == 0)
+        {
+           std::cout << "Buzz";
+           multiple = true;
+        }
+      if (!multiple)
+        {
+           std::cout << i;
+        }
+      if (i < 100)
+        {
+           std::cout << ", ";
+        }
+    }
 }

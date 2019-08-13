2841aa79c2738eea1a06344b2331aaeda2defee3
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-13 20:02:27 +0530

    Update Exercise63.cpp

--- Chapter09/Exercise63.cpp
+++ Chapter09/Exercise63.cpp
@@ -1,59 +1,22 @@
 #include <iostream> 
-
 #include <cmath> 
-
- 
-
 class Position 
-
 { 
-
     public: 
-
-        Position(float x, float y) : m_x(x), m_y(y) {} 
-
-         
-
+        Position(float x, float y) : m_x(x), m_y(y) {}          
         float distance(float x, float y) 
-
         { 
-
             float xDiff = x - m_x; 
-
             float yDiff = y - m_y; 
-
-             
-
             return std::sqrt(((xDiff * xDiff) + (yDiff * yDiff))); 
-
-        } 
-
-         
-
-    private: 
-
-         
-
+        }        
+    private:         
         float m_x; 
-
         float m_y; 
-
 }; 
-
- 
-
 int main() 
-
 { 
-
-    Position pos(10.0f, 20.0f); 
-
-     
-
-    std::cout << "The distance from pos to (100, 40) is:" << pos.distance(100.0f, 40.0f) << std::endl; 
-
-     
-
+    Position pos(10.0f, 20.0f);     
+    std::cout << "The distance from pos to (100, 40) is:" << pos.distance(100.0f, 40.0f) << std::endl;     
     return 0; 
-
 } 

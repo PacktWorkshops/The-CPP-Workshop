6765efc264b4a4ffb3c81dcc70722de2b83860d6
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-13 18:55:22 +0530

    Update Exercise21.cpp

--- Chapter04/Exercise21.cpp
+++ Chapter04/Exercise21.cpp
@@ -3,27 +3,27 @@
 #include <string>
 int main()
 {
-int numberToCheck = 0;
-std::cout << "Prime number checker\n";
-std::cout << "Enter the number you want to check: ";
-std::cin >> numberToCheck;
+  int numberToCheck = 0; 
+  std::cout << "Prime number checker\n";
+  std::cout << "Enter the number you want to check: ";
+  std::cin >> numberToCheck;
   if (numberToCheck <= 1)
-{
-std::cout << numberToCheck << " is not prime.";
-return 0;
-}
+    {
+       std::cout << numberToCheck << " is not prime.";
+       return 0;
+    }
   else if (numberToCheck == 2)
-{
-std::cout << numberToCheck << " is prime.";
-return 0;
-}
+    {
+       std::cout << numberToCheck << " is prime.";
+       return 0;
+     }
   for (int i = 2; i < numberToCheck; ++i)
-{
-if (numberToCheck % i == 0)
-{
-std::cout << numberToCheck << " is not prime.";
-return 0;
-}
-}
-std::cout << numberToCheck << " is prime.";
+    {
+       if (numberToCheck % i == 0)
+         {
+            std::cout << numberToCheck << " is not prime.";
+            return 0;
+         }
+     }
+  std::cout << numberToCheck << " is prime.";
 }

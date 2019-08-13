10f59fac255f71844d14fa7d9e159f6201d471a2
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-13 17:23:49 +0530

    Update Exercise02.cpp

--- Chapter01/Exercise02.cpp
+++ Chapter01/Exercise02.cpp
@@ -6,23 +6,23 @@
 #define GRADE_A_THRESHOLD 75
 int main()
 {
-int value = 0;
-std::cout << "Please enter test score (0 - 100): ";
-std:: cin >> value;
+  int value = 0;
+  std::cout << "Please enter test score (0 - 100): ";
+  std:: cin >> value;
   if (value < GRADE_C_THRESHOLD)
-{
-std::cout << "Fail";
-}
+    {
+      std::cout << "Fail";
+    }
   else if (value < GRADE_B_THRESHOLD)
-{
-std::cout << "Pass: Grade C";
-}
-else if (value < GRADE_A_THRESHOLD)
-{
-std::cout << "Pass: Grade B";
-}
-else
-{
-std::cout << "Pass: Grade A";
-}
+    {
+      std::cout << "Pass: Grade C";
+    }
+ else if (value < GRADE_A_THRESHOLD)
+    {
+      std::cout << "Pass: Grade B";
+    }
+ else
+    {
+      std::cout << "Pass: Grade A";
+    }
 }

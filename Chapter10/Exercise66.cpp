e45efdaf5ba0f170c5268a2e91aef03dd6fb8313
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-13 20:08:56 +0530

    Update Exercise66.cpp

--- Chapter10/Exercise66.cpp
+++ Chapter10/Exercise66.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
-
-
 class Color
 {
 public:
-std::string color = "";
-std::string GetColor() { return color; }
+  std::string color = "";
+  std::string GetColor() { return color; }
 };
 
 class Shape
 {
 public:
-int area = 10;
-int GetArea() { return area; }
+  int area = 10;
+  int GetArea() { return area; }
 };
 class Square : public Shape, public Color
 {
@@ -26,19 +24,19 @@ class Triangle : public Shape, public Color
 
 int main()
 {
-Square mySquare;
-Circle myCircle;
-Triangle myTriangle;
-mySquare.area = 5;
-mySquare.color = "red";
-std::cout << "Square Area: " << mySquare.GetArea() << std::endl;
-std::cout << "Square Color: " << mySquare.GetColor() << std::endl;
+  Square mySquare;
+  Circle myCircle;
+  Triangle myTriangle;
+  mySquare.area = 5;
+  mySquare.color = "red";
+  std::cout << "Square Area: " << mySquare.GetArea() << std::endl;
+  std::cout << "Square Color: " << mySquare.GetColor() << std::endl;
   myCircle.area = 10;
-myCircle.color = "blue";
-std::cout << "Circle Area: " << myCircle.GetArea() << std::endl;
-std::cout << "Circle Area: " << myCircle.GetColor() << std::endl;
-myTriangle.area = 15;
-myTriangle.color = "green";
-std::cout << "Triangle Area: " << myTriangle.GetArea() << std::endl;
-std::cout << "Triangle Color: " << myTriangle.GetColor() << std::endl;
+  myCircle.color = "blue";
+  std::cout << "Circle Area: " << myCircle.GetArea() << std::endl;
+  std::cout << "Circle Area: " << myCircle.GetColor() << std::endl;
+  myTriangle.area = 15;
+  myTriangle.color = "green";
+  std::cout << "Triangle Area: " << myTriangle.GetArea() << std::endl;
+  std::cout << "Triangle Color: " << myTriangle.GetColor() << std::endl;
 }

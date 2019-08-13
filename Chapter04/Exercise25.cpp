8cad8eb6afbe41f9d8e3c94b27dc53c3e809cea0
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-13 19:06:19 +0530

    Update Exercise25.cpp

--- Chapter04/Exercise25.cpp
+++ Chapter04/Exercise25.cpp
@@ -3,22 +3,24 @@
 #include <string>
 class Person
 {
-public:
-Person(int age, std::string name) :
-age(age), name(name)
-{
-};
-float age = 0;
-std::string name = "";
+  public:
+    Person(int age, std::string name) :
+    age(age), name(name)
+     {
+     };
+  float age = 0;
+  std::string name = "";
   bool operator== (Person const& other)
-{return ((age == other.age) && (name == other.name));
-}
-};
+     {
+        return ((age == other.age) && (name == other.name));
+     }
+     };
+
 int main()
 {
-Person PersonA = Person(27, "Lucy");
-Person PersonB = Person(27, "Lucy");
-Person PersonC = Person(27, "Susan");
-  std::cout << (PersonA == PersonB) << std::endl;
-std::cout << (PersonB == PersonC) << std::endl;
+   Person PersonA = Person(27, "Lucy");
+   Person PersonB = Person(27, "Lucy");
+   Person PersonC = Person(27, "Susan");
+   std::cout << (PersonA == PersonB) << std::endl;
+   std::cout << (PersonB == PersonC) << std::endl;
 }

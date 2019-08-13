8f98a2c16901522457d26e9d6cd66411ea1bbe85
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-13 17:29:46 +0530

    Update Activity01.cpp

--- Chapter01/Activity01.cpp
+++ Chapter01/Activity01.cpp
@@ -9,28 +9,28 @@
 std::string GetGroup(int age);
 int main()
 {
-std::string name = "";
-int age = 0;
-std::string group = "";
-std::cout << "Please enter your name: ";
-getline(std::cin, name);
-std::cout << "And please enter your age: ";
+  std::string name = "";
+  int age = 0;
+  std::string group = "";
+  std::cout << "Please enter your name: ";
+  getline(std::cin, name);
+  std::cout << "And please enter your age: ";
   std::cin >> age;
-group = GetGroup(age);
-std::cout << "Welcome "<< name << ". You are in " << group << ".\n";
+  group = GetGroup(age);
+  std::cout << "Welcome "<< name << ". You are in " << group << ".\n";
 }
 std::string GetGroup(int age)
 {
-if (age <= GROUP_1_THRESHOLD)
-{
-return GROUP_1_NAME;
+  if (age <= GROUP_1_THRESHOLD)
+  {
+  return GROUP_1_NAME;
+  }
+  else if (age <= GROUP_2_THRESHOLD)
+  {
+  return GROUP_2_NAME;
+  }
+  else
+  {
+  return GROUP_3_NAME;
+  }
 }
-else if (age <= GROUP_2_THRESHOLD)
-{
-return GROUP_2_NAME;
-}
-else
-{
-return GROUP_3_NAME;
-}
-};

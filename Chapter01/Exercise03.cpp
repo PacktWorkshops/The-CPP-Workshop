0bb5ad8e6c855a07a66c8bf9e03e341a0cd68b48
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-13 17:25:00 +0530

    Update Exercise03.cpp

--- Chapter01/Exercise03.cpp
+++ Chapter01/Exercise03.cpp
@@ -4,15 +4,15 @@
 int main()
 {
   std::string firstName;
-std::string lastName;
-int age;
+  std::string lastName;
+  int age;
   std::cout << "Please enter your first name(s): ";
-getline(std::cin, firstName);std::cout << "Please enter your surname: ";
-getline(std::cin, lastName);
+  getline(std::cin, firstName);std::cout << "Please enter your surname: ";
+  getline(std::cin, lastName);
   std::cout << "Please enter you age: ";
-std::cin >> age;
+  std::cin >> age;
   std::cout << std::endl;
-std::cout << "Welcome " << firstName << " " << lastName << std::endl;
-std::cout << "You are " << age << " years old." << std::endl;
-};
+  std::cout << "Welcome " << firstName << " " << lastName << std::endl;
+  std::cout << "You are " << age << " years old." << std::endl;
+}
 

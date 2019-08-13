48e318437955337f41f145dce2530d5aa838c669
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-13 20:19:10 +0530

    Update Exercise70.cpp

--- Chapter10/Exercise70.cpp
+++ Chapter10/Exercise70.cpp
@@ -1,113 +1,47 @@
 // Casting example. 
 
 #include <iostream> 
-
 #include <string> 
-
- 
-
 class Shape 
-
 { 
-
     public: 
-
-        virtual int CalculateArea() = 0; 
-
-         
-
+        virtual int CalculateArea() = 0;         
     protected: 
-
         int area = 0; 
-
-}; 
-
- 
-
+};  
 class Square : public Shape 
-
 { 
-
     public: 
-
-        int height = 0; 
-
-         
-
+        int height = 0;          
         int CalculateArea() override 
-
-        { 
-
+         { 
             area = height * height; 
-
             return area; 
-
-        } 
-
-}; 
-
- 
+         } 
+};
 
 class Circle : public Shape 
-
 { 
-
     public: 
-
-        int radius = 0; 
-
-             
-
-        int CalculateArea() override 
-
-        { 
-
+        int radius = 0;             
+        int CalculateArea() override
+         { 
             area = 3.14 * (radius * radius); 
-
             return area; 
-
-        } 
-
+         } 
 }; 
-
- 
-
 int main() 
-
 { 
-
     Shape* square = new Square(); 
-
-    Shape* circle = new Circle(); 
-
-     
-
+    Shape* circle = new Circle();      
     Square* square2 = static_cast<Square*>(square); 
-
-    square2->height = 10; 
-
-     
-
+    square2->height = 10;      
     Circle* circle2 = static_cast<Circle*>(circle); 
-
-    circle2->radius = 10; 
-
-     
-
+    circle2->radius = 10;    
     std::cout << "Square Area: " << square->CalculateArea() << std::endl; 
-
     std::cout << "Circle Area: " << circle->CalculateArea() << std::endl; 
-
- 
-
     delete square; 
-
     square = nullptr; 
-
- 
-
     delete circle; 
-
     circle = nullptr; 
-
 }

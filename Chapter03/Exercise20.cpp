ba6b9f78906a40f900c093097048f2344ab359e7
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-13 18:43:53 +0530

    Update Exercise20.cpp

--- Chapter03/Exercise20.cpp
+++ Chapter03/Exercise20.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 void MyFunc()
 {
-int myInt1 = 1;
+  int myInt1 = 1;
 }
 int main()
 {
-int myInt2 = 2;
-{
-int myInt3 = 3;
-}
+  int myInt2 = 2;
+   {
+     int myInt3 = 3;
+   }
 // print values
 std::cout << myInt1 << std::endl;
 std::cout << myInt2 << std::endl;

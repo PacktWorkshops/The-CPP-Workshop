a0022b40e42fd9aa0778e514bfab302f519ff4d2
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-13 20:12:42 +0530

    Update Exercise68.cpp

--- Chapter10/Exercise68.cpp
+++ Chapter10/Exercise68.cpp
@@ -3,36 +3,36 @@
 class Shape
 {
 public:
-virtual int CalculateArea() = 0;
+  virtual int CalculateArea() = 0;
 protected:
-int area = 0;
+  int area = 0;
 };
 class Square : public Shape
 {
 public:
-int height = 0;
-int CalculateArea() override
-{
-area = height * height;
-return area;
-}
-};
+  int height = 0;
+  int CalculateArea() override
+   {
+     area = height * height;
+     return area;
+   }
+   };
 class Circle : public Shape
 {
 public:
-int radius = 0;
-int CalculateArea() override
-{
-area = 3.14 * (radius * radius);
-return area;
-}
+     int radius = 0;
+     int CalculateArea() override
+   {
+     area = 3.14 * (radius * radius);
+     return area;
+   }
 };
 int main()
 {
-Square square;
-square.height = 10;
-std::cout << "Square Area: " << square.CalculateArea() << std::endl;
-  Circle circle;
-circle.radius = 10;
-std::cout << "Circle Area: " << circle.CalculateArea() << std::endl;
+    Square square;
+    square.height = 10;
+    std::cout << "Square Area: " << square.CalculateArea() << std::endl;
+    Circle circle;
+    circle.radius = 10;
+    std::cout << "Circle Area: " << circle.CalculateArea() << std::endl;
 }

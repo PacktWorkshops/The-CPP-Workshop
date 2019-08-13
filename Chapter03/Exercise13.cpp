4e6ac4da818cdc8b102dbaa1cfcdea1a13c2360c
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-13 18:16:06 +0530

    Update Exercise13.cpp

--- Chapter03/Exercise13.cpp
+++ Chapter03/Exercise13.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main()
 {
-int myInt = 1;
-bool myBool = false;
-char myChar = 'a';
-std::cout << "The size of an int is " << sizeof(myInt) << ".\n";
-std::cout << "The size of a bool is " << sizeof(myBool) << ".\n";
-std::cout << "The size of a char is " << sizeof(myChar) << ".\n";
-return 0;
+   int myInt = 1;
+   bool myBool = false;
+   char myChar = 'a';
+   std::cout << "The size of an int is " << sizeof(myInt) << ".\n";
+   std::cout << "The size of a bool is " << sizeof(myBool) << ".\n";
+   std::cout << "The size of a char is " << sizeof(myChar) << ".\n";
+   return 0;
 }

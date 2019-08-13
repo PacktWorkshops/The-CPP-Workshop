13abeeb36fe21b3136122c6443ff712ae53b2938
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-13 14:41:54 +0530

    Update Exercise72.cpp

--- Chapter11/Exercise72.cpp
+++ Chapter11/Exercise72.cpp
@@ -1,1 +1,48 @@
-
+#include <iostream>
+#include <typeinfo>
+using namespace std;
+template<typename T, typename U>
+class Position
+{
+public:
+Position(T x, T y, U zRot)
+{
+m_x = x;
+m_y = y;
+m_zRotation = zRot;
+}
+T const getX() { return m_x; }
+T const getY() { return m_y; }
+U const getZRotation() { return m_zRotation; }
+private:
+T m_x;
+T m_y;
+U m_zRotation;
+  };
+template<typename T>
+T getLargest(T t1, T t2)
+{
+if(t1 > t2)
+{
+return t1;
+}
+else
+{
+return t2;
+}
+}
+int main()
+{
+Position<int, float> intPosition(1, 3, 80.0f);
+Position<float, int> floatPosition(2.0f, 3.0f, 80);
+Position<long, float> longPosition(5.0, 3.0, 200.0);
+cout << "largest is: " << getLargest(intPosition.getX(), intPosition.
+getY()) << endl;
+cout << "largest is: " << getLargest(floatPosition.getX(),
+floatPosition.getY()) << endl;
+cout << "largest is: " << getLargest(longPosition.getX(),
+longPosition.getY()) << endl;
+cout << "largest ZRot is:" << getLargest(intPosition.getZRotation(),
+longPosition.getZRotation()) << endl;
+return 0;
+}

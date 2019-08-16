201adf24ccb4f70fe8aa7f0cdf76e5ab2dc30b25
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-16 18:18:10 +0530

    Update Exercise18.cpp

--- Chapter03/Exercise18.cpp
+++ Chapter03/Exercise18.cpp
@@ -1,20 +1,20 @@
 // Accessibility example.
 #include <iostream>
-#include <string>
-class MyClass
-{
-   public:
-     int myPublicInt = 0;
-     protected:
-     int myProtectedInt = 0;
-   private:
-     int myPrivateInt = 0;
- };
 
-int main()
-{
-   MyClass testClass;
-   std::cout << testClass.myPublicInt << "\n";
-   std::cout << testClass.myProtectedInt << "\n";
-   std::cout << testClass.myPrivateInt << "\n";
+#include <string>
+
+class MyClass {
+  public:
+    int myPublicInt = 0;
+  protected:
+    int myProtectedInt = 0;
+  private:
+    int myPrivateInt = 0;
+};
+
+int main() {
+  MyClass testClass;
+  std::cout << testClass.myPublicInt << "\n";
+  std::cout << testClass.myProtectedInt << "\n";
+  std::cout << testClass.myPrivateInt << "\n";
 }

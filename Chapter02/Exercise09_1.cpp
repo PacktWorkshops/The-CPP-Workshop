b6a40e1dc8f83c806bdae706bdae3c8ec75a44e0
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-16 16:53:53 +0530

    Update Exercise09_1.cpp

--- Chapter02/Exercise09_1.cpp
+++ Chapter02/Exercise09_1.cpp
@@ -1,14 +1,12 @@
 // While loop.
 #include <iostream>
+
 #include <string>
 
-int main()
-{   
-    while (false)
-    {
-        std::cout << "Hello World!";
-    }
-    
-    return 0;
-}
+int main() {
+  while (false) {
+    std::cout << "Hello World!";
+  }
 
+  return 0;
+}

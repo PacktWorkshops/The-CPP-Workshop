693a674a28265e263411d7dfcab4b3c32387d944
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-16 18:12:21 +0530

    Update Exercise15.cpp

--- Chapter03/Exercise15.cpp
+++ Chapter03/Exercise15.cpp
@@ -1,18 +1,41 @@
-// Vector example.
+// Arrays exercise.
 #include <iostream>
 #include <string>
-#include <vector>
-std::vector<int> myVector;
-void PrintVector()
-{
-   for (int i = 0; i < myVector.size(); ++i)
-     {
-        std::cout << myVector[i];
-     }
-   std::cout << "\n\n";
-}
-
+#define NAME_COUNT 5
 int main()
 {
-   PrintVector();
+   std::string names[NAME_COUNT][2] {""};
+   std::cout << "Please input usernames." << std::endl;
+   for (int i = 0; i < NAME_COUNT; ++i)
+     {
+        std::cout << "User " << i + 1 << " Forename: ";
+        std::getline(std::cin, names[i][0]);
+        std::cout << "User " << i + 1 << " Surname: ";
+        std::getline(std::cin, names[i][1]);
+     }
+   bool bIsRunning = true;
+   while (bIsRunning)
+     {
+        int userIndex = 0;
+        std::string inputString = "";
+        std::cout << "Enter user-id of user to fetch or -1 to quit: ";
+        std::getline(std::cin, inputString);
+        userIndex = std::stoi(inputString);
+        if (userIndex == -1)
+         {
+           bIsRunning = false;
+         }
+        else
+         {
+            if (userIndex >= 0 && userIndex < NAME_COUNT)
+              {
+                 std::cout << "User " << userIndex << " = "<<
+                 names[userIndex] << std::endl;
+              }
+            else
+             {
+                std::cout << "Invalid user index" << std::endl;
+              }
+          }
+      }
 }

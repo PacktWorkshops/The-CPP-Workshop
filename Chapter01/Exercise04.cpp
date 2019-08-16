fd7db3915150a1ec2906a296c249f348af821442
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-16 16:37:03 +0530

    Update Exercise04.cpp

--- Chapter01/Exercise04.cpp
+++ Chapter01/Exercise04.cpp
@@ -1,18 +1,18 @@
-int Max(int a, int b)
-{
-  if (a > b)
-    {
-      return a;
-    }
-  else
-    {
-      return b;
-    }
+// IO Exercise.
+#include <iostream>
+
+#include <string>
+
+int Max(int a, int b) {
+  if (a > b) {
+    return a;
+  } else {
+    return b;
+  }
 }
-int main()
-{
+int main() {
   int value1 = 0;
-  int value2 = 0; 
+  int value2 = 0;
   std::cout << "Please input number 1: ";
   std::cin >> value1;
   std::cout << "Please input number 2: ";

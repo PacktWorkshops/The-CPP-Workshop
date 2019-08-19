b4b5f736737a0a67dfc3c561287c180249d4c9ce
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-19 17:23:48 +0530

    Update Exercise78.cpp

--- Chapter12/Exercise78.cpp
+++ Chapter12/Exercise78.cpp
@@ -1,37 +1,39 @@
 #include <iostream>
+
 #include <string>
+
 #include <set>
+
 using namespace std;
-class Person
-{
-public:
-Person(string name, int age)
-{
-m_name = name;
-m_age = age;
-}
-string getName() const { return m_name; }
-int getAge() const { return m_age; }
-private:
-string m_name;
-int m_age;
+class Person {
+  public:
+    Person(string name, int age) {
+      m_name = name;
+      m_age = age;
+    }
+  string getName() const {
+    return m_name;
+  }
+  int getAge() const {
+    return m_age;
+  }
+  private:
+    string m_name;
+  int m_age;
 };
-struct customComparator
-{
-bool operator() (const Person& a, const Person& b) const
-{
-return (a.getAge() > b.getAge());
-}
+struct customComparator {
+  bool operator()(const Person & a,
+    const Person & b) const {
+    return (a.getAge() > b.getAge());
+  }
 };
-int main()
-{
-set<Person, customComparator> personSet;
-Person a("bob", 35);
-Person b("bob", 25);
-personSet.insert(a);
-personSet.insert(b);
-for(auto person : personSet)
-{
-cout << person.getAge() << endl;
-}
+int main() {
+  set < Person, customComparator > personSet;
+  Person a("bob", 35);
+  Person b("bob", 25);
+  personSet.insert(a);
+  personSet.insert(b);
+  for (auto person: personSet) {
+    cout << person.getAge() << endl;
+  }
 }

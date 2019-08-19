358fe47d5da9f1fd863f9a070991d196ba5dd0ff
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-08-19 13:09:14 +0530

    Update Exercise82.cpp

--- Chapter13/Exercise82.cpp
+++ Chapter13/Exercise82.cpp
@@ -1,26 +1,22 @@
 #include <iostream>
+
 #include <exception>
+
 #include <string>
+
 using namespace std;
-int deeply_nested()
-{
-string("xyzzy").at(100);
-return 0;
+int deeply_nested() {
+  string("xyzzy").at(100);
+  return 0;
 }
-int main()
-{
-try
-{
-deeply_nested();
-}
-catch (exception& e)
-{
-cout << "caught " << e.what() << endl;
-}
-catch (...)
-{
-cout << "in catch ..." << endl;
-}
-cout << "in main(), after try/catch" << endl;
-return 0;
+int main() {
+  try {
+    deeply_nested();
+  } catch (exception & e) {
+    cout << "caught " << e.what() << endl;
+  } catch (...) {
+    cout << "in catch ..." << endl;
+  }
+  cout << "in main(), after try/catch" << endl;
+  return 0;
 }

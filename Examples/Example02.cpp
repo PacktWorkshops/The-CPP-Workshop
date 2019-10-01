7b3289f430621027aaaa0c71cf5ed16018f3b27d
Adrianpackt <packtadrian@gmail.com>
2019-10-01 15:07:16 +0530

    Example02

---
+++ Examples/Example02.cpp
@@ -0,0 +1,18 @@
+// Macro example 4 – Ifdef macro.
+#include <iostream>
+#include <string>
+
+#define HELLO_WORLD "Hello World!"
+
+int main()
+{
+  #ifdef HELLO_WORLD
+     std::cout << HELLO_WORLD;
+  #endif
+
+  #undef HELLO_WORLD
+
+  #ifdef HELLO_WORLD
+     std::cout << HELLO_WORLD;
+  #endif
+}

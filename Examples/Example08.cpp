d37cb50a4d8fee06b8f5c2f060128c55aa2d63c3
Adrian Cardoza <47211725+Adrianpackt@users.noreply.github.com>
2019-10-04 12:17:40 +0530

    Add files via upload

---
+++ Examples/Example08.cpp
@@ -0,0 +1,24 @@
+// continue example.
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "Loop Starting ...\n";
+    
+    int count = 0;
+    
+    while (count < 5)
+    {
+        ++count;
+        
+        if (count == 3)
+        {
+            continue;
+        }
+        
+        std::cout << "\n" << count;
+    }
+    
+    std::cout << "\n\nLoop finished.";
+}

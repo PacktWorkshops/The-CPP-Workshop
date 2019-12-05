f3a4a0beb2935a2126b152f31c4ef05bdfa1d84a
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-12-05 13:59:49 +0530

    Update Activity11.cpp

--- Chapter11/Activity11/Activity11.cpp
+++ Chapter11/Activity11/Activity11.cpp
@@ -107,35 +107,43 @@ int main()
     testStack.push(1);
     testStack.push(2);
     cout << "stack contains values: ";
+    
     for (auto it = testStack.bottom(); it != testStack.top() + 1; ++it)
      {
         cout << *it << " ";
      }
+    
     cout << endl;
     cout << "stack contains " << testStack.size() << " elements" << endl;
     testStack.pop();
     cout << "stack contains values: ";
+    
     for (auto it = testStack.bottom(); it != testStack.top() + 1; ++it)
      {
         cout << *it << " ";
      }
+    
     cout << endl;
     cout << "stack contains " << testStack.size() << " elements" << endl;
     testStack.push(9);
     testStack.push(50);
     cout << "stack contains values: ";
+    
     for (auto it = testStack.bottom(); it != testStack.top() + 1; ++it)
      {
          cout << *it << " ";
      }
+    
     cout << endl;
     cout << "stack contains " << testStack.size() << " elements" << endl;
     testStack.pop();
     cout << "stack contains values: ";
+    
     for (auto it = testStack.bottom(); it != testStack.top() + 1; ++it)
     {
         cout << *it << " ";
     }
+    
     cout << endl;
     cout << "Is the Stack empty: " << (testStack.empty() == 1 ? "YES" : "NO") << endl;
     cout << "value of top element is: " << *testStack.top() << endl;

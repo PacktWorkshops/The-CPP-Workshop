9419364df5f3db6113246f9959041bd7460b4bd1
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2019-12-19 14:17:04 +0530

    Update Exercise25.cpp

--- Chapter04/Exercise25/Exercise25.cpp
+++ Chapter04/Exercise25/Exercise25.cpp
@@ -1,26 +1,26 @@
 // Operator Overloading Example
 #include <iostream>
 #include <string>
+
 class Person
 {
-  public:
-    Person(int age, std::string name) :
-    age(age), name(name)
-     {
-     };
-  float age = 0;
-  std::string name = "";
-  bool operator== (Person const& other)
-     {
-        return ((age == other.age) && (name == other.name));
-     }
-     };
+public:
+    Person(int age, std::string name) : age(age), name(name)
+    {
+    };
+    float age = 0;
+    std::string name = "";
+    bool operator == (Person const& other)
+    {
+         return ((age == other.age) && (name == other.name));
+    }
+};
 
 int main()
 {
-   Person PersonA = Person(27, "Lucy");
-   Person PersonB = Person(27, "Lucy");
-   Person PersonC = Person(27, "Susan");
-   std::cout << (PersonA == PersonB) << std::endl;
-   std::cout << (PersonB == PersonC) << std::endl;
+    Person PersonA = Person(27, "Lucy");
+    Person PersonB = Person(27, "Lucy");
+    Person PersonC = Person(27, "Susan");
+    std::cout << (PersonA == PersonB) << std::endl;
+    std::cout << (PersonB == PersonC) << std::endl;
 }

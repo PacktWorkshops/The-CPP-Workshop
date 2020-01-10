88f03dfb4fdae28efa9b8a25109d97e74533f185
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2020-01-10 12:07:44 +0530

    Update Exercise77.cpp

--- Chapter12/Exercise77/Exercise77.cpp
+++ Chapter12/Exercise77/Exercise77.cpp
@@ -5,17 +5,21 @@
 
 using namespace std;
 
-class Question {
+class Question 
+{
 public:
 
 	Question(int questionNumber, string question, int answerIndex) : m_questionNumber(questionNumber), m_question(question), m_answerIndex(answerIndex) {}
-	int getQuestionNumber() const {
+	int getQuestionNumber() const 
+	{
 		return m_questionNumber;
 	}
-	string getQuestion() const {
+	string getQuestion() const 
+	{
 		return m_question;
 	}
-	int getAnswerIndex() const {
+	int getAnswerIndex() const 
+	{
 		return m_answerIndex;
 	}
 
@@ -26,38 +30,48 @@ private:
 	int m_answerIndex;
 };
 
-struct QuestionCompare {
+struct QuestionCompare 
+{
 	bool operator()(const Question& lhs,
-		const Question& rhs) const {
+		const Question& rhs) const 
+	{
 		return lhs.getQuestionNumber() < rhs.getQuestionNumber();
 	}
 };
-int main() {
+
+int main() 
+{
 	map < Question, vector < string >, QuestionCompare > quiz;
 	Question question1(1, "Which two actors directed themselves in movies and won Oscars for Best Actor?", 2);
-	vector < string > question1Answers = {
+	vector < string > question1Answers = 
+	{
 	  "Al Pacino and Timothy Hutton",
 	  "Jack Nicholson and Kevin Spacey",
 	  "Laurence Olivier and Roberto Benigni",
 	  "Tom Hanks and Paul Newman"
 	};
+	
 	Question question2(2, "\"After all, tomorrow is another day!\" was the last line in which Oscar-winning Best Picture?", 0);
-	vector < string > question2Answers = {
+	vector < string > question2Answers = 
+	{
 	  "Gone With the Wind",
 	  "Great Expectations",
 	  "Harold and Maude",
 	  "The Matrix"
 	};
+	
 	quiz.insert(make_pair(question1, question1Answers));
 	quiz.insert(make_pair(question2, question2Answers));
 	cout << "Welcome to the movie quiz" << endl;
 	cout << "Type your answer between 1-4 and press enter:" << endl;
 	map < Question, vector < string >> ::iterator quizIterator = quiz.begin();
 	vector < bool > correctAnswers;
-	while (quizIterator != quiz.end()) {
+	while (quizIterator != quiz.end()) 
+	{
 		cout << quizIterator->first.getQuestion() << endl;
 		int answerIndex = 1;
-		for (auto answer : quizIterator->second) {
+		for (auto answer : quizIterator->second) 
+		{
 			cout << answerIndex << " : " << answer << endl;
 			answerIndex++;
 		}

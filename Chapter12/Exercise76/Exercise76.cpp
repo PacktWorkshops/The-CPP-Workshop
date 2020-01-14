9d0d440cc7079f04af804acb82fce2de89ec5189
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2020-01-14 18:47:33 +0530

    Update Exercise76.cpp

--- Chapter12/Exercise76/Exercise76.cpp
+++ Chapter12/Exercise76/Exercise76.cpp
@@ -1,20 +1,19 @@
 #include<iostream>
 #include<string>
 #include<algorithm>
+
 using namespace std;
 
 class Track
 {
 public:
- 
-    Track(float length, string name, int popularity) : m_trackLength(length), m_trackName(name), m_popularityRating(popularity) {}
- 
+    Track(float length, string name, int popularity) : m_trackLength(length), 
+    m_trackName(name), m_popularityRating(popularity) {}
     float getLength() const { return m_trackLength; }
     string getName() const { return m_trackName; }
     int getPopularity() const { return m_popularityRating; }
  
 private:
- 
     float m_trackLength;
     string m_trackName;
     int m_popularityRating;
@@ -22,22 +21,22 @@ private:
 
 bool trackLengthCompare(const Track& t1, const Track& t2)
 {
-return (t1.getLength() < t2.getLength());
+    return (t1.getLength() < t2.getLength());
 }
 
 int main()
 { 
     vector<Track> tracks;
- 
+    
     tracks.push_back(Track(199.0f, "God's Plan", 100));
     tracks.push_back(Track(227.0f, "Hold On, We're Going Home", 95));
     tracks.push_back(Track(182.0f, "The Motto", 80));
- 
     sort(tracks.begin(), tracks.end(), trackLengthCompare);
  
     for (auto t : tracks)
     {
         cout << t.getName() << endl;
     }
+    
     return 0;
 }

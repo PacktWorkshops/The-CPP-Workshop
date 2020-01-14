69621ab9796ae541a792d17708729691d3dc793e
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2020-01-14 17:24:02 +0530

    Update Exercise16.cpp

--- Chapter03/Exercise16/Exercise16.cpp
+++ Chapter03/Exercise16/Exercise16.cpp
@@ -2,17 +2,19 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
 std::vector<int> myVector;
+
 void PrintVector()
 {
    for (int i = 0; i < myVector.size(); ++i)
-     {
-       std::cout << myVector[i];
-     }
+   {
+      std::cout << myVector[i];
+   }
    std::cout << "\n\n";
- }
+}
 
 int main()
 {
-PrintVector();
+   PrintVector();
 }

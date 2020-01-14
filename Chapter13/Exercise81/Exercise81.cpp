bc64148e0b05db64fad617dc67f8373e908734f9
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2020-01-14 18:56:45 +0530

    Update Exercise81.cpp

--- Chapter13/Exercise81/Exercise81.cpp
+++ Chapter13/Exercise81/Exercise81.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 
 using namespace std;
-int deeply_nested() {
-  throw 123;
-  return 0;
+
+int deeply_nested() 
+{
+    throw 123;
+    return 0;
 }
 
-int main() {
-  try {
-    deeply_nested();
-  } catch (...) {
-    cout << "in catch ..." << endl;
-  }
-  cout << "in main(), after try/catch" << endl;
-  return 0;
+int main() 
+{
+    try 
+    {
+        deeply_nested();
+    } 
+    catch (...) 
+    {
+        cout << "in catch ..." << endl;
+    }
+    cout << "in main(), after try/catch" << endl;
+    
+    return 0;
 }

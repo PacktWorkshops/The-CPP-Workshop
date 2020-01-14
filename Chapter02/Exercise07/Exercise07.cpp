14ab71078f5445c7076b05d9e3a92dcb6fc103bc
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2020-01-14 19:30:02 +0530

    Update Exercise07.cpp

--- Chapter02/Exercise07/Exercise07.cpp
+++ Chapter02/Exercise07/Exercise07.cpp
@@ -13,22 +13,20 @@ int main()
     std::cout << "Please enter a number 1-3 to view an item price: ";
     getline (std::cin, input);
     number = std::stoi(input);
-        switch (number)
-         {
-                 case 1:
-                 std::cout << "Fries: $0.99\n";
-                 break;
-        
-                 case 2:
-                 std::cout << "Burger: $1.25\n";
-                 break;
-        
-                 case 3:
-                 std::cout << "Shake: $1.50\n";
-                 break;
-
-                 default:
-                 std::cout << "Invalid choice.";
-                 break;
+    
+    switch (number)
+    {
+        case 1:
+            std::cout << "Fries: $0.99\n";
+            break;
+        case 2:
+             std::cout << "Burger: $1.25\n";
+             break;
+        case 3:
+             std::cout << "Shake: $1.50\n";
+             break;
+        default:
+            std::cout << "Invalid choice.";
+            break;
           }
 }

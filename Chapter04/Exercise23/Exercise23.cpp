4755f894f13ab78d75d5f2daa3d3f982996907fa
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2020-01-14 19:42:13 +0530

    Update Exercise23.cpp

--- Chapter04/Exercise23/Exercise23.cpp
+++ Chapter04/Exercise23/Exercise23.cpp
@@ -1,12 +1,13 @@
 // Pre/Post Increment Example.
 #include <iostream>
 #include <string>
+
 int main()
- {
-   int myInt = 5;
-   std::cout << ++myInt << std::endl;
-   std::cout << myInt << std::endl;
-   myInt = 5;
-   std::cout << myInt++ << std::endl;
-   std::cout << myInt << std::endl;
+{
+    int myInt = 5;
+    std::cout << ++myInt << std::endl;
+    std::cout << myInt << std::endl;
+    myInt = 5;
+    std::cout << myInt++ << std::endl;
+    std::cout << myInt << std::endl;
  }

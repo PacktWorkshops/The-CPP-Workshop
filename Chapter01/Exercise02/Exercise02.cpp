56050d06243f7efe48fc76269c36ecaeeb2ef899
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2020-01-14 19:19:56 +0530

    Update Exercise02.cpp

--- Chapter01/Exercise02/Exercise02.cpp
+++ Chapter01/Exercise02/Exercise02.cpp
@@ -1,21 +1,30 @@
 // Preprocessor directives activity.
 #include <iostream>
-
 #include <string>
 
-#
-define GRADE_C_THRESHOLD 25# define GRADE_B_THRESHOLD 50# define GRADE_A_THRESHOLD 75
-int main() {
-  int value = 0;
-  std::cout << "Please enter test score (0 - 100): ";
-  std::cin >> value;
-  if (value < GRADE_C_THRESHOLD) {
-    std::cout << "Fail";
-  } else if (value < GRADE_B_THRESHOLD) {
-    std::cout << "Pass: Grade C";
-  } else if (value < GRADE_A_THRESHOLD) {
-    std::cout << "Pass: Grade B";
-  } else {
-    std::cout << "Pass: Grade A";
-  }
+#define GRADE_C_THRESHOLD 25
+#define GRADE_B_THRESHOLD 50
+#define GRADE_A_THRESHOLD 75
+
+int main() 
+{
+    int value = 0;
+    std::cout << "Please enter test score (0 - 100): ";
+    std::cin >> value;
+    if (value < GRADE_C_THRESHOLD)
+    {
+        std::cout << "Fail";
+    } 
+    else if (value < GRADE_B_THRESHOLD)
+    {
+        std::cout << "Pass: Grade C";
+    } 
+    else if (value < GRADE_A_THRESHOLD) 
+    {
+        std::cout << "Pass: Grade B";
+    } 
+    else 
+    {
+        std::cout << "Pass: Grade A";
+    }
 }

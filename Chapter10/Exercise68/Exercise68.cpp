29797f11a21e671073084208e157a2617df67cb7
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2020-01-14 18:14:25 +0530

    Update Exercise68.cpp

--- Chapter10/Exercise68/Exercise68.cpp
+++ Chapter10/Exercise68/Exercise68.cpp
@@ -1,32 +1,37 @@
 #include <iostream>
 #include <string>
+
 class Shape
 {
 public:
-  virtual int CalculateArea() = 0;
+    virtual int CalculateArea() = 0;
+
 protected:
-  int area = 0;
+    int area = 0;
 };
+
 class Square : public Shape
 {
 public:
-  int height = 0;
-  int CalculateArea() override
-   {
-     area = height * height;
-     return area;
-   }
-   };
+    int height = 0;
+    int CalculateArea() override
+    {
+        area = height * height;
+        return area;
+    }
+ };
+
 class Circle : public Shape
 {
 public:
      int radius = 0;
      int CalculateArea() override
    {
-     area = 3.14 * (radius * radius);
-     return area;
+       area = 3.14 * (radius * radius);
+       return area;
    }
 };
+
 int main()
 {
     Square square;

5f8a4c170681102341595ca5386f19b27fb26caf
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2020-01-14 18:43:17 +0530

    Update Exercise75.cpp

--- Chapter12/Exercise75/Exercise75.cpp
+++ Chapter12/Exercise75/Exercise75.cpp
@@ -1,28 +1,18 @@
 #include <iostream>
-
 #include <vector>
 
 using namespace std;
 
-int main() {
-  vector < int > vec = {
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10
-  };
-
-  for (auto v: vec) {
-    cout << v << " ";
-  }
-
-  cout << vec[3];
-
-  return 0;
+int main()
+{
+    vector < int > vec = {1,2,3,4,5,6,7,8,9,10};
+    
+    for (auto v: vec) 
+    {
+        cout << v << " ";
+    }
+    
+    cout << vec[3];
+    
+    return 0;
 }

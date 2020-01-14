10e70b6895f3237fedc9ff7776ba73d7e2cd70d7
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2020-01-14 18:05:59 +0530

    Update Exercise65.cpp

--- Chapter10/Exercise65/Exercise65.cpp
+++ Chapter10/Exercise65/Exercise65.cpp
@@ -1,27 +1,32 @@
 #include <iostream>
+
 class Shape
 {
 public:
-  int area = 10;
-  int GetArea() { return area; }
+    int area = 10;
+    int GetArea() { return area; }
 };
+
 class Square : public Shape
 {
 };
+
 class Circle : public Shape
 {
 };
+
 class Triangle : public Shape
 {
 };
+
 int main()
 {
-  Square mySquare;
-  Circle myCircle;
-  Triangle myTriangle;
-  mySquare.area = 5;
-  std::cout << "Square Area: " << mySquare.GetArea() << std::endl;
-  myCircle.area = 15;
-  std::cout << "Circle Area: " << myCircle.GetArea() << std::endl;
-  std::cout << "Triangle Area: " << myTriangle.GetArea() << std::endl;
+    Square mySquare;
+    Circle myCircle;
+    Triangle myTriangle;
+    mySquare.area = 5;
+    std::cout << "Square Area: " << mySquare.GetArea() << std::endl;
+    myCircle.area = 15;
+    std::cout << "Circle Area: " << myCircle.GetArea() << std::endl;
+    std::cout << "Triangle Area: " << myTriangle.GetArea() << std::endl;
 }

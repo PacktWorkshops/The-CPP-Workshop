fa1bcfdd7f7adb9ab7d5abd0c24bcd6bfb0934cb
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2020-01-14 18:50:05 +0530

    Update Exercise77.cpp

--- Chapter12/Exercise77/Exercise77.cpp
+++ Chapter12/Exercise77/Exercise77.cpp
@@ -8,12 +8,12 @@ using namespace std;
 class Question 
 {
 public:
-
 	Question(int questionNumber, string question, int answerIndex) : m_questionNumber(questionNumber), m_question(question), m_answerIndex(answerIndex) {}
 	int getQuestionNumber() const 
 	{
 		return m_questionNumber;
 	}
+	
 	string getQuestion() const 
 	{
 		return m_question;
@@ -24,7 +24,6 @@ public:
 	}
 
 private:
-
 	int m_questionNumber;
 	string m_question;
 	int m_answerIndex;
@@ -45,19 +44,19 @@ int main()
 	Question question1(1, "Which two actors directed themselves in movies and won Oscars for Best Actor?", 2);
 	vector < string > question1Answers = 
 	{
-	  "Al Pacino and Timothy Hutton",
-	  "Jack Nicholson and Kevin Spacey",
-	  "Laurence Olivier and Roberto Benigni",
-	  "Tom Hanks and Paul Newman"
+		"Al Pacino and Timothy Hutton",
+	    "Jack Nicholson and Kevin Spacey",
+	    "Laurence Olivier and Roberto Benigni",
+	    "Tom Hanks and Paul Newman"
 	};
 	
 	Question question2(2, "\"After all, tomorrow is another day!\" was the last line in which Oscar-winning Best Picture?", 0);
 	vector < string > question2Answers = 
 	{
-	  "Gone With the Wind",
-	  "Great Expectations",
-	  "Harold and Maude",
-	  "The Matrix"
+		"Gone With the Wind",
+	    "Great Expectations",
+	    "Harold and Maude",
+	    "The Matrix"
 	};
 	
 	quiz.insert(make_pair(question1, question1Answers));
@@ -66,24 +65,29 @@ int main()
 	cout << "Type your answer between 1-4 and press enter:" << endl;
 	map < Question, vector < string >> ::iterator quizIterator = quiz.begin();
 	vector < bool > correctAnswers;
+	
 	while (quizIterator != quiz.end()) 
 	{
 		cout << quizIterator->first.getQuestion() << endl;
 		int answerIndex = 1;
+		
 		for (auto answer : quizIterator->second) 
 		{
 			cout << answerIndex << " : " << answer << endl;
 			answerIndex++;
 		}
+		
 		int answer;
 		cin >> answer;
 		int correctAnswer = quizIterator->first.getAnswerIndex();
 		bool wasCorrect = answer - 1 == correctAnswer;
 		cout << (wasCorrect ? "CORRECT!" : "INCORRECT!") << " Correct answer is: " << quizIterator->second[correctAnswer] << endl;
+		
 		if (wasCorrect)
 			correctAnswers.push_back(answer);
 		quizIterator++;
 	}
+	
 	cout << "Your score was " << correctAnswers.size() << " out of " << quiz.size() << endl;
 	cout << "done";
 }

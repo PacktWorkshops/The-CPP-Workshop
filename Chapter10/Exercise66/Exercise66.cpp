9fdb79c41ff006b992c6227f5deff26ff171fc09
MaheshPackt <43748254+MaheshPackt@users.noreply.github.com>
2020-01-14 18:08:01 +0530

    Update Exercise66.cpp

--- Chapter10/Exercise66/Exercise66.cpp
+++ Chapter10/Exercise66/Exercise66.cpp
@@ -2,41 +2,44 @@
 class Color
 {
 public:
-  std::string color = "";
-  std::string GetColor() { return color; }
+    std::string color = "";
+    std::string GetColor() { return color; }
 };
 
 class Shape
 {
 public:
-  int area = 10;
-  int GetArea() { return area; }
+    int area = 10;
+    int GetArea() { return area; }
 };
+
 class Square : public Shape, public Color
 {
 };
+
 class Circle : public Shape, public Color
 {
 };
+
 class Triangle : public Shape, public Color
 {
 };
 
 int main()
 {
-  Square mySquare;
-  Circle myCircle;
-  Triangle myTriangle;
-  mySquare.area = 5;
-  mySquare.color = "red";
-  std::cout << "Square Area: " << mySquare.GetArea() << std::endl;
-  std::cout << "Square Color: " << mySquare.GetColor() << std::endl;
-  myCircle.area = 10;
-  myCircle.color = "blue";
-  std::cout << "Circle Area: " << myCircle.GetArea() << std::endl;
-  std::cout << "Circle Area: " << myCircle.GetColor() << std::endl;
-  myTriangle.area = 15;
-  myTriangle.color = "green";
-  std::cout << "Triangle Area: " << myTriangle.GetArea() << std::endl;
-  std::cout << "Triangle Color: " << myTriangle.GetColor() << std::endl;
+    Square mySquare;
+    Circle myCircle;
+    Triangle myTriangle;
+    mySquare.area = 5;
+    mySquare.color = "red";
+    std::cout << "Square Area: " << mySquare.GetArea() << std::endl;
+    std::cout << "Square Color: " << mySquare.GetColor() << std::endl;
+    myCircle.area = 10;
+    myCircle.color = "blue";
+    std::cout << "Circle Area: " << myCircle.GetArea() << std::endl;
+    std::cout << "Circle Color: " << myCircle.GetColor() << std::endl;
+    myTriangle.area = 15;
+    myTriangle.color = "green";
+    std::cout << "Triangle Area: " << myTriangle.GetArea() << std::endl;
+    std::cout << "Triangle Color: " << myTriangle.GetColor() << std::endl;
 }

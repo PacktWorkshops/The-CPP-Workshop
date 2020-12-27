e70912d61f5815e4ae1699f979e9714f99da08d9
shravan gatty <57343821+shravan-gatty@users.noreply.github.com>
2020-12-27 14:02:27 +0530

    update Exercise04.cpp
    we can use std header file in top instead of using std in every single line

--- Chapter01/Exercise04/Exercise04.cpp
+++ Chapter01/Exercise04/Exercise04.cpp
@@ -1,6 +1,7 @@
 // IO Exercise.
 #include <iostream>
 #include <string>
+using namespce std;
 
 int Max(int a, int b) 
 {
@@ -18,9 +19,9 @@ int main()
 {
     int value1 = 0;
     int value2 = 0;
-    std::cout << "Please input number 1: ";
-    std::cin >> value1;
-    std::cout << "Please input number 2: ";
-    std::cin >> value2;
-    std::cout << "The highest number is " << Max(value1, value2);
+    cout << "Please input number 1: ";
+    cin >> value1;
+    cout << "Please input number 2: ";
+    cin >> value2;
+    cout << "The highest number is " << Max(value1, value2);
 }
